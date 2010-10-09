e356e626ab3a350b4d1003026f0223922aa02e9f
xbmcadult <xbmcadult@gmail.com>
2010-10-09 01:53:18 +0000

    added plugin.video.VideoDevil

---
+++ plugin.video.VideoDevil/default.py
@@ -0,0 +1,26 @@
+import xbmc, xbmcgui
+import sys, os, re
+import urllib, urllib2
+
+__plugin__ = 'VideoDevil'
+__author__ = 'sfaxman'
+__url__ = 'http://code.google.com/p/xbmc-addons/'
+__svn_url__ = 'http://xbmc-addons.googlecode.com/svn/trunk/plugins/video/VideoDevil/'
+__credits__ = 'bootsy'
+__version__ = '1.6.2'
+
+rootDir = os.getcwd()
+if rootDir[-1] == ';':rootDir = rootDir[0:-1]
+
+class Main:
+    def __init__(self):
+        self.pDialog = None
+        self.curr_file = ''
+        self.run()
+
+    def run(self):
+            import videodevil
+            videodevil.Main()
+            #sys.modules.clear()
+
+win = Main()
